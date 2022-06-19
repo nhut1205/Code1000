e7a6708cea1172afd8aeaa459b44d0c3868d66b7
nhut1205 <104210356+nhut1205@users.noreply.github.com>
2022-06-19 22:31:23 +0700

    Update TryCatch.cpp

--- TryCatch.cpp
+++ TryCatch.cpp
@@ -8,25 +8,25 @@ int main()
     scanf("%d%d%d", &a, &b, &c);
     try
     {
-        if (c == 0)
+        if (c == 0) //neu c = 0 nhan loi 101
             throw 101;
-        if (a = 0 && b == 0 && c != 0)
-            throw 102;
-        else
+        if (a = 0 && b == 0 && c != 0) // neu a va b = 0 c khac 0 nhan loi 102
+            throw 102; 
+        else //thuc hien phep tinh
             t = ((float)a - b) / c;
     }
     catch (int Error)
     {
-        if (Error == 101)
+        if (Error == 101) // tha loi 101 VD a = 5 b = 4 c = 0 (5-4)/0 loi vi khong so nao chia het cho 0 duoc
         {
             printf("Loi chia cho khong\n");
         }
-        if (Error == 102)
+        if (Error == 102)// tha loi 102 VD a = 0 , b = 0 , c = 7 0/7 = 0 0 chia moi so deu bang 0
         {
             printf("Ket qua : 0");
         }
     }
-    printf("Ket qua : %.2f", t);
+    printf("Ket qua : %.2f", t);// hien thi phep tinh , nho la kieu float de tra ket qua dung
     getch();
     return 0;
 }
